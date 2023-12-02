3c8736b3729770da13adeb2958f5a576bce1bab3
ser9330026 <ser9330026@yandex.ru>
2023-12-02 09:55:57 +0500

    Добавьте файлы проекта.

---
+++ Multi-files progects 1/Multi-files progects 1.cpp
@@ -0,0 +1,22 @@
+﻿////Создайте многофайловый проект на C++, который содержит 
+////основной файл main.cpp и дополнительный файл с функциями
+////utilities.cpp.Функции из utilities.cpp должны выполнять
+////различные математические операции, такие как сложение, 
+////вычитание, умножение и деление.
+
+#include <iostream>
+#include "Sum.h"
+#include "MyClass.h"
+
+int main() {
+	setlocale(LC_ALL, "rus");
+
+	
+	std::cout << Sum(3, 6) << std::endl;
+	MyNamespace::MyClass m;//Подключаем класс к нашему собственному пространству имен
+	//m.printMassage("иии");
+
+
+
+	return 0;
+}
